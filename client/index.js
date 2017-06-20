49bb8f2792eb0fe099bfab54975f8ec49ea43648
gilbertjacke <gilbertjacke@gmail.com>
2017-06-20 09:49:08 +0100

    added react

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log("webpack live and configured")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
