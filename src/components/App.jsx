fd54bcd5912a0e2bb2cfb47b42edbf6c0c967990
gilbertjacke <gilbertjacke@gmail.com>
2017-06-21 12:14:54 +0100

    changed: client to src in webpack.config

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+// import HeroImages from './HeroImages';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Shortlist Studio</h1>
+        <h4>Hero Images</h4>
+        {/* <HeroImages /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/HeroImages.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import axios from 'axios';
+// import '../../dummydata.json';
+//
+// class HeroImages extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       heroImages: []
+//     };
+//   }
+//
+//
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hero Image Component</h1>
+//         <h4>Hero Images</h4>
+//         <ul>
+//
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default HeroImages;
