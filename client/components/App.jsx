41a248fac4cf0b153e21e5c805afd97261edabfe
gilbertjacke <gilbertjacke@gmail.com>
2017-06-21 11:45:30 +0100

    added dummydata

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import axios from 'axios';
+import '../../dummydata.json';
 
-export default class App extends React.Component {
-  render(){
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      heroImages: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`../../dummydata.acf.hero_images.url`)
+      .then(res => {
+        const posts = res.data.data.children.map(obj => obj.data);
+        this.setState({ heroImages });
+      });
+  }
+
+  // console.log(this.state)
+
+  render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
+      <div>
+        <h1>Shortlist Studio</h1>
+        <h4>Hero Images</h4>
+
+        <ul>
+          {this.state.map(heroImage =>
+            <li key={heroImage.id}>{heroImage.ur}</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
+
+export default App;
